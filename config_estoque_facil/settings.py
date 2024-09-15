5602b52ecc7dcbde598092b3b7fa9c85f1ac3fb0
felipe01daniel <fdaniels.it@gmail.com>
2024-09-14 22:05:09 -0300

    Implementação do PostgreSQL no projeto Django

--- config_estoque_facil/settings.py
+++ config_estoque_facil/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!wlsmj@(_&lj03@vm*4_7i*&x7@)(g&!@xx#00*i%v55ju*tf_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'estoque_facil',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'config_estoque_facil.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'estoque_facil',
+        'USER': 'postgres',
+        'PASSWORD': '@testeImpacta',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
