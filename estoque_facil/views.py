8b54a0e66a2680a873806e6032eb453e3aeeb11b
felipe01daniel <fdaniels.it@gmail.com>
2024-09-16 15:41:03 -0300

    Adicionado filtro de produtos por nome e categoria na listagem

--- estoque_facil/views.py
+++ estoque_facil/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProdutoForm
-from .models import Produto
+from .models import Produto, Categoria
 
 # Create your views here.
 def adicionar_produto(request):
@@ -27,5 +27,23 @@ def editar_produto(request, produto_id):
     return render(request, 'editar_produto.html', {'form': form, 'produto': produto})
 
 def listar_produtos(request):
+    nome_query = request.GET.get('nome', '')
+    categoria_query = request.GET.get('categoria', '')
+
     produtos = Produto.objects.all()
-    return render(request, 'listar_produtos.html', {'produtos': produtos})
+
+    # Filtro por nome
+    if nome_query:
+        produtos = produtos.filter(nome__icontains=nome_query)
+
+    # Filtro por categoria
+    if categoria_query:
+        produtos = produtos.filter(categoria__id=categoria_query)
+
+    categorias = Categoria.objects.all()
+    return render(request, 'listar_produtos.html', {
+        'produtos': produtos,
+        'categorias': categorias,
+        'nome_query': nome_query,
+        'categoria_query': categoria_query
+    })
